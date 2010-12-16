3fa4a2dc38f92e41ef0a5ad15cd886ad47d12b65
viteinfinite <viteinfinite@gmail.com>
2010-12-16 13:15:37 -0800

    Fixed crash: "Popovers cannot be presented from a view which does not have a window."

--- Classes/MGSplitViewController.m
+++ Classes/MGSplitViewController.m
@@ -576,8 +576,9 @@
 		
 	} else if (!inPopover && _hiddenPopoverController && _barButtonItem) {
 		// I know this looks strange, but it fixes a bizarre issue with UIPopoverController leaving masterViewController's views in disarray.
+if (self.view.window != nil) {
 		[_hiddenPopoverController presentPopoverFromRect:CGRectZero inView:self.view permittedArrowDirections:UIPopoverArrowDirectionAny animated:NO];
-		
+		}
 		// Remove master from popover and destroy popover, if it exists.
 		[_hiddenPopoverController dismissPopoverAnimated:NO];
 		[_hiddenPopoverController release];
